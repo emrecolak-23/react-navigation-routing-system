7f2417f3649cd8e2908466fa4a95abdcfcc31f86
Emre Çolak <emrecolak@emrecolak.local>
2023-01-03 21:47:13 +0300

    dropdown page and navigation context created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Blue', value: 'blue' },
-    { label: 'Green', value: 'green' },
-  ];
-
-  return (
-    <Dropdown options={options} value={selection} onChange={handleSelect} />
-  );
+  return <div>App</div>;
 }
 
 export default App;
