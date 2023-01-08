97c0354889d5e001a4059028bb363ab87e58c84f
Emre Çolak <emrecolak@emrecolak.local>
2023-01-08 16:01:34 +0300

    make reusable sorting hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,30 +1,15 @@
-import { useState } from 'react';
 import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from './Table';
 
+import useSort from '../hooks/use-sort';
+
 function SortableTable(props) {
   const { config, data } = props;
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
 
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortBy(label);
-      setSortOrder('asc');
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder('asc');
-      setSortBy(label);
-    } else if (sortOrder === 'asc') {
-      setSortOrder('desc');
-      setSortBy(label);
-    } else if (sortOrder === 'desc') {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { sortBy, sortOrder, setSortColumn, sortedData } = useSort(
+    data,
+    config
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -37,7 +22,7 @@ function SortableTable(props) {
         return (
           <th
             className="cursor-pointer hover:bg-gray-100"
-            onClick={() => handleClick(column.label)}
+            onClick={() => setSortColumn(column.label)}
           >
             <div className="flex items-center">
               {getIcons(column.label, sortBy, sortOrder)}
@@ -52,25 +37,6 @@ function SortableTable(props) {
   // Only sort data if sortOrder && sortBy are not null
   // Make a copy of the 'data' prop
   // Find the correct sortValue function and use it for sort
-  let sortedData = data;
-  if (sortBy && sortOrder) {
-    const { sortValue } = config.find((column) => {
-      return column.label === sortBy;
-    });
-
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-      if (typeof valueA === 'string') {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   const getIcons = (label, sortBy, sortOrder) => {
     if (label != sortBy) {
---
+++ src/pages/CounterPage.js
@@ -0,0 +1,18 @@
+import Button from '../components/Button';
+
+import useCounter from '../hooks/use-counter';
+
+function CounterPage({ initialCount }) {
+  const { count, increment } = useCounter(initialCount);
+
+  return (
+    <div>
+      Count is {count}
+      <Button secondary rounded onClick={increment}>
+        Increment
+      </Button>
+    </div>
+  );
+}
+
+export default CounterPage;
